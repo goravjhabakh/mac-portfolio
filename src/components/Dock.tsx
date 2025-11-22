3cf2e238274484789dcaa339aeb58b203fa90775
Gorav Jhabakh <developer.gor05@gmail.com>
2025-11-23 04:48:57 +0530

    dock hover animation

---
+++ src/components/Dock.tsx
@@ -0,0 +1,77 @@
+import { useRef } from "react"
+import { dockApps } from "../constants"
+import { Tooltip } from "react-tooltip"
+import gsap from "gsap"
+import { useGSAP } from "@gsap/react"
+
+const Dock = () => {
+  const dockRef = useRef<HTMLDivElement>(null)
+
+  useGSAP(() => {
+    const dock = dockRef.current
+    if (!dock) return
+
+    const icons = dock.querySelectorAll('.dock-icon')
+    
+    const animateIcons = (mouseX: number) => {
+      const { left } = dock.getBoundingClientRect()
+      icons.forEach((icon) => {
+        const { left: iconLeft, width } = icon.getBoundingClientRect()
+        const center = iconLeft - left + width / 2
+        const distance = Math.abs(mouseX - center)
+        const intensity = Math.exp(-(distance ** 2.5) / 20000)
+
+        gsap.to(icon, {
+          scale: 1 + 0.25 * intensity,
+          y: -15 * intensity,
+          duration: 0.2,
+          ease: "power2.out",
+        })
+      })
+    }
+
+    const handleMoveMouse = (e: MouseEvent) => {
+      const { left } = dock.getBoundingClientRect()
+      animateIcons(e.clientX - left)
+    }
+
+    const resetIcons = () => icons.forEach((icon) => {
+      gsap.to(icon, {
+        scale: 1,
+        y: 0,
+        duration: 0.3,
+        ease: "power1.out",
+      })
+    })
+
+    dock.addEventListener("mousemove", handleMoveMouse)
+    dock.addEventListener("mouseleave", resetIcons)
+
+    return () => {
+      dock.removeEventListener("mousemove", handleMoveMouse)
+      dock.removeEventListener("mouseleave", resetIcons)
+    }
+  }, [])
+
+  const toggleApp = ({ id, canOpen }: { id: string, canOpen: boolean }) => {
+
+  }
+
+  return (
+    <section id="dock">
+      <div ref={dockRef} className="dock-container">
+        {dockApps.map(({ id, name, img, canOpen }) => (
+          <div key={id} className="relative flex justify-center">
+            <button type="button" className="dock-icon" aria-label={name} data-tooltip-id="dock-tooltip" 
+              data-tooltip-content={name} data-tooltip-delay-show={150} disabled={!canOpen} onClick={() => toggleApp({ id, canOpen })}>
+              <img src={img} alt={name} loading="lazy" className={canOpen ? "" : "opacity-50"}/>  
+            </button>
+          </div>
+        ))}
+
+        <Tooltip id="dock-tooltip" place="top" className="tooltip "/>
+      </div>
+    </section>
+  )
+}
+export default Dock
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -11,4 +11,43 @@ const navIcons = [
   { id:4, img: '/images/profile.svg' },
 ]
 
-export { navLinks, navIcons }
+const dockApps = [
+  {
+    id: 'finder',
+    name: 'Resume',
+    img: '/images/finder.png',
+    canOpen: true
+  },
+  {
+    id: 'safari',
+    name: 'Portfolio',
+    img: '/images/safari.png',
+    canOpen: true
+  },
+  {
+    id: 'photos',
+    name: 'Gallery',
+    img: '/images/photos.png',
+    canOpen: false
+  },
+  {
+    id: 'contacts',
+    name: 'Contact',
+    img: '/images/contact.png',
+    canOpen: true
+  },
+  {
+    id: 'terminal',
+    name: 'Skills',
+    img: '/images/terminal.png',
+    canOpen: false
+  },
+  {
+    id: 'trash',
+    name: 'Archive',
+    img: '/images/trash.png',
+    canOpen: false
+  }
+]
+
+export { navLinks, navIcons, dockApps }
